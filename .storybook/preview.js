468dfca7bd1ff3f422e8bfc9c2e6d5d60ac490d5
Michael Chan <mijoch@gmail.com>
2021-09-21 07:55:50 -0700

    de-optimize image NextImage in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,17 @@
 import "../styles/globals.css";
 
+// https://github.com/vercel/next.js/issues/18393#issuecomment-909636489
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage {...props} unoptimized loader={({ src }) => src} />
+  ),
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
