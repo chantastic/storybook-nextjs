6eebd6788d921ba4f0a2cd947b6912cee110f91f
Matt Sutkowski <msutkowski@gmail.com>
2021-09-28 20:53:35 -0700

    Add types, reorganize worker

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,21 @@
 import "../styles/globals.css";
-import { start } from "./initialize_worker";
-
 // https://github.com/vercel/next.js/issues/18393#issuecomment-909636489
 import * as NextImage from "next/image";
+import { setupWorker, rest } from "msw";
 
-start();
+// Make sure we're in a browser
+if (typeof global.process === "undefined") {
+  const worker = setupWorker();
+  // Apply default handlers... should be from a setup collection somewhere in a real implementation
+  worker.start(
+    rest.get("http://localhost:3000/api/hello", (req, res, ctx) => {
+      return res(ctx.json({ name: "John Doe" }));
+    })
+  );
+
+  // Assign it to the window for re-use in stories
+  window.msw = { worker, rest };
+}
 
 const OriginalNextImage = NextImage.default;
 
